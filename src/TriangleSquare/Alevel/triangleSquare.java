b213b82bb87b1674f61bd57f7ef102f4417c2b83
Tiomix <layeraz@gmail.com>
2020-01-19 12:05:44 +0200

    Second commit

--- src/TriangleSquare/Alevel/triangleSquare.java
+++ src/TriangleSquare/Alevel/triangleSquare.java
@@ -2,23 +2,20 @@ package TriangleSquare.Alevel;
 
 public class triangleSquare {
     public static void main(String[] args) {
-        double x1 = 2,
-                x2 = 1,
-                x3 = -6;
 
-        double y1 = -3,
-                y2 = 1,
-                y3 = 5;
+        double x1 = 2;
+        double x2 = 1;
+        double x3 = -6;
+        double y1 = -3;
+        double y2 = 1;
+        double y3 = 5;
 
         System.out.println("Даны точки A(" + x1 + "," + y1 + ")  " + "B(" + x2 + "," + y2 + ")  " + "C(" + x3 + "," + y3 + ")");
         System.out.println("Вычислим площадь треугольника по заданным координатам");
 
         double S = ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) / 2;
 
-
         System.out.println();
         System.out.println("Площадь треугольника =  " + S + " кв.ед");
-
     }
-
 }
